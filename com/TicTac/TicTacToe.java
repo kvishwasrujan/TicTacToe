8ee9a3dfd0770f1ba42518f0552014359fe66106
kvishwasrujan <roohithbandi00@gmail.com>
2020-10-08 13:04:01 +0530

    Added Toss

--- com/TicTac/TicTacToe.java
+++ com/TicTac/TicTacToe.java
@@ -65,6 +65,31 @@ public class TicTacToe {
 		return board[location] == ' ';
 	}
 
+	/**
+	 * UC5 Show position of the move
+	 * 
+	 * @param board
+	 * @param location
+	 * @param letter
+	 */
+	private static void Move(char[] board, int location, char letter) {
+		boolean spacefree = isSpaceFree(board, location);
+		if (spacefree)
+			board[location] = letter;
+	}
+
+	/**
+	 * Toss to decide
+	 */
+	private void toss() {
+		int num = (int)  Math.floor(Math.random() * 10) % 2;
+		if (num == 1) {
+			System.out.println("User will play first");
+		} else {
+			System.out.println("Computer will play first");
+		}
+	}
+
 	public static void main(String args[]) {
 		TicTacToe b = new TicTacToe();
 		char[] board = b.createBoard();
@@ -79,5 +104,8 @@ public class TicTacToe {
 		System.out.println("Computer letter is : " + computerLetter);
 		TicTacToe.showBoard(board);
 		int choosenMove = moveLocation(board, userInput);
+		Move(board, choosenMove, userLetter);
+		showBoard(board);
+		b.toss();
 	}
 }
